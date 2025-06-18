015527da30c0dde3b3b231fa903c11e39ccac432
D4ni3lsan0502 <215027722+D4ni3lsan0502@users.noreply.github.com>
2025-06-18 17:21:59 +0000

    Corrige dependências

--- push.sh
+++ push.sh
@@ -1,29 +1,4 @@
-1. #!/bin/bash
-git add .
-git commit -m "Corrige dependências"
-git pushLimpar node_modules
-rm -rf node_modules package-lock.json
-2. Reinstalar
-npm install
-3. Commitar
 #!/bin/bash
-
-# Adiciona todas as alterações
-git add .
-
-# Faz commit das alterações
-git commit -m "Corrige dependências"
-
-# Faz push para o repositório remoto
-git push
-
-# Limpa node_modules e package-lock.json
-rm -rf node_modules package-lock.json
-
-# Reinstala as dependências
-npm install
-
-# Adiciona e faz commit das alterações após reinstalar dependências
 git add .
 git commit -m "Corrige dependências"
 git push
